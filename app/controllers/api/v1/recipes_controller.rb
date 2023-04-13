68b508fd896ab9c6c656062887346baf314698d5
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-04-13 10:50:28 -0400

    setting up recipes view and recipe view

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::RecipesController < ApplicationController
+  before_action :set_recipe, only: %i[show destroy]
   def index
     recipe = Recipe.all.order(created_at: :desc)
     render json: recipe
@@ -14,15 +15,22 @@ class Api::V1::RecipesController < ApplicationController
   end
 
   def show
+    render json: @recipe
   end
 
   def destroy
+    @recipe&.destroy
+    render json: { message: 'Recipe deleted!'}
   end
 
   private
 
   def recipe_params
       params.permit(:name, :image, :ingredients, :instruction)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 
 end
